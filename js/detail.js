ca8bf33ce2f6b46ed856144e8561402c43cb0cdf
keremsakarya <sakaryakerem252@gmail.com>
2024-06-21 18:00:23 +0300

    first commit

---
+++ js/detail.js
@@ -0,0 +1,46 @@
+import { menu } from "./db.js"
+import { calculatePrice, elements } from "./helpers.js"
+/*
+ * URL de ki parametreleri yönetilbek için URLSearchParams class ından örnek oluşturduk
+ * Örneği oluştururken kendi URL mizde ki parametreleri gönderdik
+ */
+const search = window.location.search
+const searchParams = new URLSearchParams(search)
+//* Get yöntemine gönderdiğimiz değişkene göre değere ulaştık
+const paramid = searchParams.get("id")
+//* URL den aldığımız paramid değişkenini number a çevirdik ve sonrasında bu id li elemanı dizi içerisinden bulduk
+const product = menu.find((item) => item.id === Number(paramid))
+
+console.log(elements.outlet)
+
+elements.outlet.innerHTML = `
+    <div class="d-flex justify-content-between align-items-center">
+        <a href="/index.html">
+            <i class="bi bi-house fs-1"></i>
+        </a>
+        <div>home / ${product.category
+    } / ${product.title.toLowerCase()}</div>
+    </div>
+        <h1 class="text-center my-3 shadow p-2 rounded">${product.title}</h1>
+    <div class="d-flex justify-content-center align-items-center">
+        <img
+        style="max-width: 480px"
+        class="img-fluid shadow rounded"
+        src="${product.img}"
+        alt=""
+        />
+    </div>
+    <div>
+    <h3 class="my-5">
+    Product's category: <span class="text-success">${product.title}</span>
+    </h3>
+    <h3>Product's price: <span class="text-success">${calculatePrice(
+        product.price
+    )} ₺</span></h3>
+    </div>
+    <p class="lead fs-3">
+    ${product.desc}
+    </p>
+
+
+`
